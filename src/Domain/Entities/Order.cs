dc64eae88c3a42cba333cdb29c8fc1fba24e12c6
Felipe paez <felipepaezmouthon@gmail.com>
2026-03-25 16:52:03 -0500

    Refactor Order class properties and method

--- src/Domain/Entities/Order.cs
+++ src/Domain/Entities/Order.cs
@@ -1,19 +1,15 @@
-using System;
-using System.Collections.Generic;
-
 namespace Domain.Entities;
 
 public class Order
 {
-    public int Id;
-    public string CustomerName;
-    public string ProductName;
-    public int Quantity;
-    public decimal UnitPrice;
+    public int Id { get; set; }
+    public string CustomerName { get; set; }
+    public string ProductName { get; set; }
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
 
-    public void CalculateTotalAndLog()
+    public decimal CalculateTotal()
     {
-        var total = Quantity * UnitPrice; 
-        Infrastructure.Logging.Logger.Log("Total (maybe): " + total);
+        return Quantity * UnitPrice;
     }
 }
