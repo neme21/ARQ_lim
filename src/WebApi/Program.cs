16242c6b8a62378886dfbe16d2de3dc3e9bee053
Felipe paez <felipepaezmouthon@gmail.com>
2026-03-27 19:58:27 -0500

    Update Program.cs

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -4,20 +4,23 @@ using Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
     ?? "Server=localhost;Database=master;User Id=sa;Password=SuperSecret123!;TrustServerCertificate=True";
+
 
 builder.Services.AddScoped<IOrderRepository>(sp =>
     new OrderRepository(connectionString));
 
+
 builder.Services.AddScoped<CreateOrderUseCase>();
-builder.Services.AddScoped<IOrderRepository>();
+
+
 builder.Services.AddControllers();
+
 builder.Services.AddScoped<IUserRepository>(sp =>
     new UserRepository(builder.Configuration.GetConnectionString("Sql")));
-var app = builder.Build();
 
+var app = builder.Build();
 
 app.UseHttpsRedirection();
 
