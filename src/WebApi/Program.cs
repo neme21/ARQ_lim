4c8dfb69b9ec6fe86fc7260576c8c230c0cf259b
Felipe paez <felipepaezmouthon@gmail.com>
2026-03-29 21:04:43 -0500

    Refactor Program.cs for connection string and Swagger
    Updated connection string retrieval and added user repository registration. Enhanced Swagger setup for development environment.

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -4,20 +4,32 @@ using Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("Sql");
+var connectionString = builder.Configuration.GetConnectionString("Sql")
+    ?? throw new InvalidOperationException("Connection string not found.");
 
 builder.Services.AddScoped<IOrderRepository>(sp =>
-    new OrderRepository(connectionString!));
+    new OrderRepository(connectionString));
+
+builder.Services.AddScoped<IUserRepository>(sp =>
+    new UserRepository(connectionString));
 
 builder.Services.AddScoped<CreateOrderUseCase>();
 
 builder.Services.AddControllers();
 
-builder.Services.AddScoped<IUserRepository>(sp =>
-    new UserRepository(connectionString!));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseHttpsRedirection();
+
 app.MapControllers();
+
 await app.RunAsync();
