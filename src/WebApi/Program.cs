79f423cd21de72036445db7e689b3b5360785ade
Felipe paez <felipepaezmouthon@gmail.com>
2026-03-29 20:16:53 -0500

    Modify default connection string in Program.cs
    Updated connection string to use a default value.

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -5,7 +5,7 @@ using Infrastructure.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
-    ?? "Server=localhost;Database=master;User Id=sa;Password=SuperSecret123!;TrustServerCertificate=True";
+    
 
 
 builder.Services.AddScoped<IOrderRepository>(sp =>
